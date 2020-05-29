ba833777642ba049b397293be2fcfd3fad6b9475
hariharanragothaman <hariharanragothaman@gmail.com>
2020-05-29 14:58:50 -0400

    GCD-LCM and BFS / DFS tricks

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -14,11 +14,40 @@ class Graph:
         """
     # General strategy for BFS  - Works for undirected-graph
     def BFS(self, start):
-        pass
+        visited = {}
+        result_path = []
+        for k in self.graph:
+            visited[k] = False
+
+        q = deque(start)
+        while q:
+            vertex = q.popleft()
+            for neighbours in self.graph[vertex]:
+                if visited[neighbours] is False:
+                    visited[neighbours] = True
+                    q.append(neighbours)
+            result_path.append(vertex)
+        return result_path
+
+    def DFS(self, start):
 
     # General strategy for DFS - Works for undirected graph
     def DFS(self, start):
-        pass
+        visited = {}
+        result_path = []
+        for k in self.graph:
+            visited[k] = False
+
+        stack = [start, ]
+        visited[start] = True
+
+        q= deque(start)
+        while q:
+            vertex = q.popleft()
+            for neighbours in self.graph[vertex]:
+                if visited[neighbours] is False:
+                    stack.append(neighbours)
+        return result_path
 
     # To find paths, given start and end
     def find_path(self, start, end, path=[]):
@@ -32,6 +61,25 @@ class Graph:
                 new_path = self.find_path(node, end, path)
                 return new_path
         return None
+
+    # Template to find all paths
+    def find_all_paths(self, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in self.graph:
+            return None
+        paths = []
+
+        for node in self.graph[start]:
+            if node not in path:
+                new_paths = self.find_all_paths(node, end, path)
+                for np in new_paths:
+                    paths.append(np)
+        return paths
+
+
+
 
 # Given edges check if there is a cycle
 
--- maths/math_tricks.py
+++ maths/math_tricks.py
@@ -1,6 +1,17 @@
 from functools import reduce
 
 
+# GCD and LCM of b/w 2 numbers
+def gcd(a, b):
+    while b != 0:
+        a, b = a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
 # To find all factors of a give number
 def factors(n):
     return set(reduce(list.__add__,
@@ -13,12 +24,15 @@ matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 matrix[:] = zip(*matrix)  # Getting Transpose [ R->C, C->R ]
 matrix[:] = zip(*matrix[::-1])  # Rotate a matrix - Transpose & reverse each row
 
+
 # To get the rows of the matrix:
 def get_rows(matrice):
     return [[c for c in r] for r in matrice]
 
+
 def get_columns(matrice):
     return zip(*matrice)
+
 
 # Reversing an integer
 def reverse_integer(num):
@@ -29,12 +43,12 @@ def reverse_integer(num):
     while x:
         # Getting the LSB-  multiply by 10 every time;
         # since it will be correct as we add digits
-        result = (x%10) + result*10
+        result = (x % 10) + result * 10
         # Remove the LSB
         x = x // 10
 
     # Handle overflow bound scenario
-    if x > 2**31+1 or x < -2**31-1:
+    if x > 2 ** 31 + 1 or x < -2 ** 31 - 1:
         return 0
 
     return result * sign
