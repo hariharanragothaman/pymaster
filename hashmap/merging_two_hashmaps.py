2009c06ce0b8b2e23146f5fb738413083d698cf8
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-18 14:15:22 -0400

    Merge 2 hashmaps recursively

---
+++ hashmap/merging_two_hashmaps.py
@@ -0,0 +1,64 @@
+def test_merge_hmaps():
+    a = {
+        'a': 1,
+        'b': {
+            'b1': 2,
+            'b2': 3,
+        },
+    }
+
+    b = {
+        'a': 1,
+        'b': {
+            'b1': 4,
+        },
+    }
+    assert merge_hmaps(a, b)['a'] == 1
+    assert merge_hmaps(a, b)['b']['b2'] == 3
+    assert merge_hmaps(a, b)['b']['b1'] == 4
+
+
+import collections
+from collections import abc
+
+
+def merge_hmaps(dct, merge_dct, add_keys=True):
+    """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+
+    This version will return a copy of the dictionary and leave the original
+    arguments untouched.
+
+    The optional argument ``add_keys``, determines whether keys which are
+    present in ``merge_dict`` but not ``dct`` should be included in the
+    new dict.
+
+    Args:
+        dct (dict) onto which the merge is executed
+        merge_dct (dict): dct merged into dct
+        add_keys (bool): whether to add new keys
+
+    Returns:
+        dict: updated dict
+    """
+    dct = dct.copy()
+    if not add_keys:
+        merge_dct = {
+            k: merge_dct[k]
+            for k in set(dct).intersection(set(merge_dct))
+        }
+
+    for k, v in merge_dct.items():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.abc.Mapping)):
+            dct[k] = merge_hmaps(dct[k], merge_dct[k], add_keys=add_keys)
+        else:
+            dct[k] = merge_dct[k]
+
+    return dct
+
+
+if __name__ == '__main__':
+    test_merge_hmaps()
--- strings/rabin_karp.py
+++ strings/rabin_karp.py
@@ -5,6 +5,7 @@ Solution Approach:
 """
 from functools import reduce
 
+
 def rabin_karp(s, t):
     if len(s) > len(t):
         return -1
@@ -12,23 +13,28 @@ def rabin_karp(s, t):
     base = 26
 
     # Base hash codes computing for 's' and 't'
-    t_hash = reduce(lambda h, c: h * base + ord(c), t [:len(s)], 0)
+    t_hash = reduce(lambda h, c: h * base + ord(c), t[:len(s)], 0)
     s_hash = reduce(lambda h, c: h * base + ord(c), s, 0)
-    power_s = base ** max(len(s)-1, 0) # base ^ |s-1|
+
+    print("The thash is:", t_hash)
+    print("The shash is:", s_hash)
+
+    power_s = base ** max(len(s) - 1, 0)  # base ^ |s-1|
+    print("The power_s is:", power_s)
 
     for i in range(len(s), len(t)):
-
         # Check for hashcode and actual string to avoid collision
-        if t_hash == s_hash and t[i-len(s):i] == s:
+        if t_hash == s_hash and t[i - len(s):i] == s:
             return i - len(s)
 
-        t_hash -= ord(t[i-len(s)]) * power_s
+        t_hash -= ord(t[i - len(s)]) * power_s
         t_hash = t_hash * base + ord(t[i])
 
     if t_hash == s_hash and t[-len(s):] == s:
         return len(t) - len(s)
 
     return -1
+
 
 if __name__ == '__main__':
     t = 'GACGCCA'
