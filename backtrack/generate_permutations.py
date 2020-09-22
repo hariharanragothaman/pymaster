50d297368773aa7c0fe1e803b583116303e1aae7
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-22 11:07:35 -0400

    Adding commonly used backtracking logic and a unix folder

--- backtrack/generate_permutations.py
+++ backtrack/generate_permutations.py
@@ -1,5 +1,17 @@
 """
-File focuses on generating permutations
+File focuses on generating permutations:
+
+Given a collection of distinct numbers, return all possible permutations.
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
 """
 
 """
---
+++ backtrack/unique_permutations.py
@@ -0,0 +1,32 @@
+"""
+Given a collection of numbers that might contain duplicates,
+return all possible unique permutations.
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+def generate_unique_permute(arr):
+    """
+    The go-to can also putting them in a set when generating all permutations
+    Now, lets look into a smarter way of doing this.
+    """
+    result = [[]]
+    for n in arr:
+        temp = []
+        for l in result:
+            for i in range(len(l)+1):
+                temp.append(l[:i] + [n] + l[i:])
+                if i < len(l) and l[i] == n:
+                    break
+        result = temp
+    return result
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    op = generate_unique_permute(nums)
+    print(op)
