a245b503f10fa8400eeb965deee4114a039a363f
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-22 10:28:36 -0400

    Generating all permutations

---
+++ backtrack/generate_permutations.py
@@ -0,0 +1,36 @@
+"""
+File focuses on generating permutations
+"""
+
+"""
+Generate permutations - The classic DFS way
+"""
+def generate_permutations(arr):
+    result = []
+    if len(arr) == 0:
+        return []
+    if len(arr) == 1:
+        return [arr]
+    for i in range(len(arr)):
+        extract = arr[i]
+        remaining = arr[:i] + arr[i+1:]
+        for p in generate_permutations(remaining):
+            result.append(p + [extract])
+    return result
+
+
+"""
+Generate permutations by yield an iterator.
+"""
+def gen_permute_iter(arr):
+    if len(arr) < 1:
+        yield arr
+    else:
+        for perm in gen_permute_iter(arr[1:])
+            for i in range(len(arr)):
+                yield perm[:i] + arr[0:1] + perm[i:]
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    op = generate_permutations(nums)
+    print("The op is:", op)
