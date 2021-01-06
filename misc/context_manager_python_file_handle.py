8980672dbd041501bfb1b6180f026d5601ad753c
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-06 04:43:22 -0500

    Adding context manager basics:

---
+++ misc/context_manager_python_file_handle.py
@@ -0,0 +1,24 @@
+# To define an Class for Opening a file
+
+class OpenFile:
+    def __init__(self, filename, mode):
+        print("Entering the constructor")
+        self.file_name = filename
+        self.mode = mode
+        self.file = None
+        return
+
+    def __enter__(self):
+        print("Calling the __enter__ method")
+        self.file_handler = open(self.file_name, self.mode)
+        return self.file_handler
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Calling __exit__ method", exc_type, exc_val, exc_tb)
+        if self.file_handler:
+            self.file_handler.close()
+        return
+
+with OpenFile('test.txt', 'r') as f:
+    print("Reading a file")
+    print(f.read())
