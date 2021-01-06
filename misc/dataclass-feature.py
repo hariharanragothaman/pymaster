b548221a246926f6b0df2126cd2112b7fb499340
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-06 05:45:23 -0500

    Learning about dataclass feature

---
+++ misc/dataclass-feature.py
@@ -0,0 +1,16 @@
+"""
+A data class is effectively a class whose sole purpose is
+to literally hold data. The class will have variables that can be
+accessed and written to, but there is no extra logic on top of it.
+"""
+
+from dataclasses import dataclass
+
+@dataclass
+class Vector3D:
+    x: int
+    y: int
+    z: int
+
+u = Vector3D(1, 1, -1)
+print(u)
