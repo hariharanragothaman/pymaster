119921abea745d95be9b937129f4c417eac58b74
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-06 04:46:27 -0500

    Context manager as a generator

---
+++ misc/context_manager_as_generator.py
@@ -0,0 +1,13 @@
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(filename, mode):
+    try:
+        # Line #7 == __enter__
+        f = open(filename, mode)
+        yield f
+    finally:
+        f.close()
+
+with open_file('test.txt', 'r') as f:
+    print(f.read())
