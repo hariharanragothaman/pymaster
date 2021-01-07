3aebe3014ef7ad43dadc5a8d994c27ae9420c0a2
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-07 00:02:35 -0500

    Compute the closest square root using binary search

---
+++ maths/compute_closest_square_root.py
@@ -0,0 +1,22 @@
+"""
+Write a program which takes a non-negative integer and retursn the largest integer whose aquare
+is less than or equal to the given integer.
+16 - 4
+300 - 17  { 17^2 - 289 and 289 < 300}
+"""
+
+# Linear approach is bruteforce, will take more than 10^9 iterations for 32 bit integer.
+
+def compute_square_root(k):
+    # Obviously we have to use binary search
+    left, right = 0, k
+    while left <= right:
+        pivot = (left + right) // 2
+        if pow(pivot, 2) <= k:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+    return left - 1
+
+op = compute_square_root(300)
+print(op)
