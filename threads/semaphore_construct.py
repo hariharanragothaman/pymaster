ad3208c7f7d68fb41b8e2a43465f9c38a05d40a7
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-06 23:38:39 -0500

    Adding a semaphore construct

---
+++ threads/semaphore_construct.py
@@ -0,0 +1,31 @@
+"""
+Parallel computing benefits:
+High Performance, Better use of resources, fairness, convenience, Fault Tolerance
+
+Challenges:
+race conditions - two concurrent instruction sequences access the same address in memory , and atleast one of them
+                  writes to that address
+
+starvation, deadlock, livelock
+"""
+
+import threading
+
+class Semaphore:
+    def __init__(self, max_available):
+        self.cv = threading.Condition()
+        self.MAX_AVAILABLE = max_available
+        self.taken = 0
+
+    def acquire(self):
+        self.cv.acquire()
+        while self.cv == self.MAX_AVAILABLE:
+            self.cv.wait()
+        self.taken += 1
+        self.cv.release()
+
+    def release(self):
+        self.cv.acquire()
+        self.taken -= 1
+        self.cv.notify()
+        self.cv.release()
