4086a26b94066e5a8d9640bd412ba80e7f287e31
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-28 16:21:39 -0500

    Local vs global variables in python

---
+++ general/local_vs_global.py
@@ -0,0 +1,19 @@
+# Case 1: nolocal is used when we need to access a variable in a nested function
+
+def func1():
+    x = 5
+    def func2():
+        nonlocal x
+        print(x)
+    func2()
+
+func1()
+
+# More common problematic scenario - where we think it's global but it's not.
+x = 5
+def func3():
+    # Add this line, actually makes it reference the globally declared x
+    global x
+    x += 5
+
+func3()
