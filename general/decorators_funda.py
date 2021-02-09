b4e4a70d1f1df242097d2486cd82583d2ab7460d
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-09 18:35:50 -0500

    Adding functionality for arguments

--- general/decorators_funda.py
+++ general/decorators_funda.py
@@ -2,15 +2,17 @@ import time
 
 def logging_time(func):
     """ Decorator that logs the time """
-    def logger():
+    def logger(*args, **kwargs):
         start = time.time()
-        func()
+        func(*args, **kwargs)
         print(f"Calling {func.__name__} : {time.time() - start:.5f}")
-    
+
     return logger
 
 @logging_time
 def calculate_sum():
+    """Function to calculate sum"""
     return sum(range(100000))
 
 calculate_sum()
+print(calculate_sum.__doc__)
