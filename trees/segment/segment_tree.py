7a53d466232e9b169c9fb1799814650778ad7bdb
Hariharan Ragothaman <hariharanragothaman@users.noreply.github.com>
2021-02-28 23:31:00 -0500

    Range-query, segment tree usage - min /max function (#1)
    Updating logic for merging hashmaps
    
    Template for segment-tree and fenwick tree

--- trees/segment/segment_tree.py
+++ trees/segment/segment_tree.py
@@ -20,3 +20,51 @@ Another solution is:
 How can we perform both operations in O(logn) time?
 
 """
+
+class SegmentTree:
+    def __init__(self, arr, function):
+        self.tree = [None for _ in range(len(arr))] + arr
+        self.size = len(arr)
+        self.fn = function
+        self.build_tree()
+
+    def build_tree(self):
+        for i in range(self.size - 1, 0, -1):
+            self.tree[i] = self.fn(self.tree[i * 2], self.tree[i * 2 + 1])
+
+    def update(self, p, v):
+        p += self.size
+        self.tree[p] = v
+        while p > 1:
+            p = p // 2
+            self.tree[p] = self.fn(self.tree[p * 2], self.tree[p * 2 + 1])
+
+    def query(self, l, r):
+        l, r = l + self.size, r + self.size
+        res = None
+        while l <= r:
+            if l % 2 == 1:
+                res = self.tree[l] if res is None else self.fn(res, self.tree[l])
+            if r % 2 == 0:
+                res = self.tree[r] if res is None else self.fn(res, self.tree[r])
+            l, r = (l + 1) // 2, (r - 1) // 2
+        return res
+
+    def get_sum(self, start, stop):
+        start += self.size
+        stop += self.size
+
+        result = 0
+
+        while start <= stop:
+            if start % 2 == 1:
+                result += self.tree[start+1]
+
+
+
+if __name__ == '__main__':
+    arr = [5, 8, 6, 3, 2, 7, 2, 6]
+    seg_obj = SegmentTree(arr, min)
+    print(seg_obj.tree)
+    result = seg_obj.query(2, 7)
+    print("The result is: ", result)
