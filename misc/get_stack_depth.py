ec479b2aad3656dfa73186924c33b21aef15206e
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-03-11 21:46:12 -0500

    Adding function to get stack_depth()

---
+++ misc/get_stack_depth.py
@@ -0,0 +1,10 @@
+
+import inspect
+
+
+def stack_depth():
+    """
+    Quick function to get the stack depth
+    Returns:
+    """
+    return len(inspect.getouterframes(inspect.currentframe())) - 1
