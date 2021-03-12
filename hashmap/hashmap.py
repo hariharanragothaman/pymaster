f610649317f53cb40ab28af936ebd8b7dedd42d3
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-03-11 21:46:36 -0500

    Using setdefault() function

--- hashmap/hashmap.py
+++ hashmap/hashmap.py
@@ -32,6 +32,15 @@ d['something-else'].append("not relevant")
 d['python'].append("language")
 print(d)
 
-# Setdefault feature
-hmap = {}
+# Setdefault feature example
+# String transforms to another string problem?
+def can_convert(s1, s2):
+    """ Convert 2 strings of same length by doing zero or more conversions"""
+    if s1 == s2:
+        return True
+    dp = {}
+    for i, j in zip(s1, s2):
+        if dp.setdefault(i, j) != j:
+            return False
+    return len(set(s2)) < 26
 
