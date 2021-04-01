074c2c2dba094134537be2602794c9ea745c3d99
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-01 02:17:40 -0400

    Adding basic template for trie datastructure

--- datastructures/hashmap/merging_two_hashmaps.py
+++ datastructures/hashmap/merging_two_hashmaps.py
@@ -1,6 +1,3 @@
-
-
-
 import collections
 from collections import abc
 
--- datastructures/trie/trie.py
+++ datastructures/trie/trie.py
@@ -1,6 +1,36 @@
-# Example class for Trie
-
 class Trie:
     def __init__(self, *words):
         self.root = dict()
-        
+        for word in words:
+            self.add(word)
+
+    def add(self, word):
+        print("The word is:", word)
+        current_dict = self.root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, dict())
+        current_dict["_end_"] = True
+
+    def __contains__(self, word):
+        current_dict = self.root
+        for letter in word:
+            if letter not in current_dict:
+                return False
+            current_dict = current_dict[letter]
+        return "_end_" in current_dict
+
+    def __delitem__(self, word):
+        current_dict = self.root
+        nodes = [current_dict]
+        for letter in word:
+            current_dict = current_dict[letter]
+            nodes.append(current_dict)
+        del current_dict["_end_"]
+
+
+if __name__ == '__main__':
+    words = ["hello", "world", "wow"]
+    t = Trie(*words)
+    print(t.root)
+    res = t.__contains__('wow')
+    print(res)
