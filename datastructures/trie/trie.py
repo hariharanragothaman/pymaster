1b00a0a12a50d95afab06e5a5922138a756c94d1
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-01 02:24:33 -0400

    Adding starts_with though __contains__ takes care of it

--- datastructures/trie/trie.py
+++ datastructures/trie/trie.py
@@ -27,10 +27,20 @@ class Trie:
             nodes.append(current_dict)
         del current_dict["_end_"]
 
+    def starts_with(self, prefix):
+        current_dict = self.root
+        for letter in prefix:
+            current = current_dict.get(letter)
+            if current is None:
+                return False
+            current_dict = current_dict[letter]
+        return True
 
 if __name__ == '__main__':
     words = ["hello", "world", "wow"]
     t = Trie(*words)
     print(t.root)
     res = t.__contains__('wow')
+    print(res)
+    res = t.starts_with('worl')
     print(res)
