f92cc7e89fc03d607f0ceefd8f611589e0f320bb
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-25 16:35:39 -0400

    Update iter_intro.py

--- pyfundae/itertools/iter_intro.py
+++ pyfundae/itertools/iter_intro.py
@@ -1,11 +1,6 @@
 import itertools
 import operator
 
-# To flatten a nested list:
-list1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-flt = list(itertools.chain.from_iterable((list1)))
-print(flt)
-
 # To consecutively multiply elements
 arr = [1, 2, 3, 4, 5, 6]
 op = list(itertools.accumulate(arr, operator.mul))
