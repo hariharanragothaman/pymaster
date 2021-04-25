5c84fc12f7a1e7225ef95ca39c40c1814e9bea3b
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-25 13:22:55 -0400

    Logic has been consumed by another file

--- maths/get_diagonals.py
+++
@@ -1,7 +0,0 @@
-# Storing the left diagonals in a hashmap
-for i in range(rows):
-    hashmap[(i, i)] = mat[i][i]
-
-# Storing the right diagonals in a hashmap
-for i in range(rows):
-    hashmap[(rows - 1 - i, i)] = mat[rows - 1 - i][i]
