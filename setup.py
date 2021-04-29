ef8f6b9f6df779607675b5271d35a692b69121f2
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-29 15:18:05 -0400

    Making pymaster into a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymaster",
+    packages=find_packages(exclude=['tests', 'docs']),
+    version='1.0.0',
+    description='Quick Recipes for interview problems',
+    author='Hariharan Ragothaman',
+    classifiers=[
+            'Topic:: Utilities',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 3',
+        ],)
