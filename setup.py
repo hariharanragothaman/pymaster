e381aad8977b5f7889f057d40af2fa759bbe6db1
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-29 16:40:34 -0400

    Polishes to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymaster",
-    packages=find_packages(exclude=['tests', 'docs']),
-    version='1.0.0',
-    description='Quick Recipes for interview problems',
-    author='Hariharan Ragothaman',
+    packages=find_packages(exclude=["tests", "docs"]),
+    version="1.0.0",
+    description="Quick Recipes for interview problems",
+    author="Hariharan Ragothaman",
     classifiers=[
-            'Topic:: Utilities',
-            'Operating System :: POSIX',
-            'Programming Language :: Python :: 3',
-        ],)
+        "Topic:: Utilities",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+    ],
+)
