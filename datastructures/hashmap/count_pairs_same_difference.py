f8589b2dd65db539e919d76bbb4f0cc031ff6174
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-05 14:46:18 -0400

    Count pairs that have the same difference j-i == pos(j) - pos(i)

---
+++ datastructures/hashmap/count_pairs_same_difference.py
@@ -0,0 +1,21 @@
+def solve(arr):
+    hash_map = {}
+    for i, c in enumerate(arr):
+        hash_map[c - i] = hash_map.get(c - i, 0) + 1
+
+    count = 0
+    for k in hash_map:
+        n = hash_map[k]
+        count += (n * (n - 1) // 2)
+
+    print(count)
+
+
+if __name__ == '__main__':
+    t = int(input())
+    i = 0
+    while i < t:
+        n = int(input())
+        arr = list(map(int, input().split()))
+        solve(arr)
+        i += 1
