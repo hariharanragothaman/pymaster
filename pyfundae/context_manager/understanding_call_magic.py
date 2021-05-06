512e3f1a3f8d14d65f3b6d58498dfd59937fac37
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-06 11:32:41 -0400

    understanding the __call__ method

---
+++ pyfundae/context_manager/understanding_call_magic.py
@@ -0,0 +1,13 @@
+class FooBar:
+    def __init__(self, sample_string):
+        self.s = sample_string
+
+    def __call__(self, *args, **kwargs):
+        """ Since this takes arguments, we can pass whatever we want into this """
+        print("Entering the call object")
+
+
+if __name__ == '__main__':
+    f = FooBar('example')
+    # So the instance is allowed to behave a function
+    f()
