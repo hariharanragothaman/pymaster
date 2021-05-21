e1a3318d57fe089d5df424cc564feadb8b80e194
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-20 23:33:03 -0400

    Checking in fully optimized solution

--- datastructures/arrays/problems/minimum_swaps_to_make_sorted.py
+++ datastructures/arrays/problems/minimum_swaps_to_make_sorted.py
@@ -16,8 +16,33 @@ def find_min_swaps_to_sort(arr):
             start += 1
     print(i)
 
+def find_min_swaps_to_sort_slightly_optimized(arr):
+    """ This solution TLE'ed - we need to optimize this"""
+    start = 1
+    end = len(arr)
+
+    hmap = {}
+    for i, val in enumerate(arr):
+        hmap[val] = i
+
+    i = 0
+    while start <= end:
+        if arr[start-1] != start:
+            idx = hmap[start]
+            arr[idx], arr[start-1] = arr[start-1], arr[idx]
+
+            # Updating the hashmap as well - which is like our database
+            hmap[arr[idx]] = idx
+            hmap[arr[start-1]] = start-1
+
+
+            i += 1
+        start += 1
+    print(i)
+
+# This is TLE'ing I guess because of finding the index of it() - Oh! interesting
 
 
 if __name__ == '__main__':
     arr = list(map(int, input().split()))
-    find_min_swaps_to_sort(arr)
+    find_min_swaps_to_sort_slightly_optimized(arr)
