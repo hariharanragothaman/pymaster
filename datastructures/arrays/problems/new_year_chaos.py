f67dc76070ac025ec97399c21d1cc80caeaa8c24
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-23 21:33:42 -0400

    Some Minor Polishes

--- datastructures/arrays/problems/new_year_chaos.py
+++ datastructures/arrays/problems/new_year_chaos.py
@@ -1,7 +1,18 @@
+"""
+https://www.hackerrank.com/challenges/new-year-chaos
+"""
+
 def find_min_bribes(arr):
     n = len(arr)
+    bribes = 0
     for i in range(n-1, -1, -1):
-        pass
+        if arr[i] - (i+1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0, arr[i]-2), i):
+            if arr[j] > arr[i]:
+                bribes += 1
+    print(bribes)
 
 
 if __name__ == '__main__':
