50b5c733cf6e83ae4b92a19ca1d516418ab1eae9
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-07-02 10:16:11 -0400

    Get nth fibonacci numner - there are much more efficient methods to find this.

---
+++ algorithms/dynamic/get_nth_fibonacci_number_bottom_up.py
@@ -0,0 +1,24 @@
+import resource, sys
+
+resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, -1))
+sys.setrecursionlimit(10 ** 7)
+
+def get_nth_fibonacci(n):
+    cache = [0, 1]
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    i = 2
+    while i <= n:
+        tmp = cache[-1]
+        cache[-1] = cache[-1] + cache[-2]
+        cache[-2] = tmp
+        i += 1
+    return cache[-1]
+
+if __name__ == '__main__':
+    n = int(input())
+    res = get_nth_fibonacci(n)
+    mod = 10**9 + 7
+    print(res % mod)
