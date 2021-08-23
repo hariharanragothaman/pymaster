feb84f13b29fdbf266299a9b4b8264c9a85feb6b
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-23 00:23:39 -0400

    Adding problem description

--- graphs/problems/520B_shortest_path.py
+++ graphs/problems/520B_shortest_path.py
@@ -5,7 +5,14 @@ from math import ceil, floor, log, log2, sqrt, gcd, factorial, pow, pi
 from bisect import bisect_left, bisect_right
 
 start_time = time.time()
-
+"""
+Vasya has found a strange device. On the front panel of a device there are: a red button, a blue button and a display showing some positive integer. 
+After clicking the red button, device multiplies the displayed number by two. 
+After clicking the blue button, device subtracts one from the number on the display. 
+If at some point the number stops being positive, the device breaks down. The display can show arbitrarily large numbers. 
+Initially, the display shows number n.
+Bob wants to get number m on the display. What minimum number of clicks he has to make in order to achieve this result?
+"""
 
 def main():
     """
