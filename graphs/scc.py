27b99fb936b10b5433b78ff5cfbd32c35af13633
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-09-29 04:09:13 -0400

    Adding tarjans algorithm

---
+++ graphs/scc.py
@@ -0,0 +1,29 @@
+"""
+Given a directed graph, return a list of lists, containing
+the strongly connected components in topological order
+"""
+
+def find_strongly_connected_components(graph):
+    SCC, S, P = [], [], []
+    depth = [0] * len(graph)
+
+    stack = list(range(len(graph)))
+    while stack:
+        node = stack.pop()
+        if node < 0:
+            d = depth[~node] - 1
+            if P[-1] > d:
+                SCC.append(S[d:])
+                del S[d:], P[-1]
+                for node in SCC[-1]:
+                    depth[node] = -1
+        elif depth[node] > 0:
+            while P[-1] > depth[node]:
+                P.pop()
+        elif depth[node] == 0:
+            S.append(node)
+            P.append(len(S))
+            depth[node] = len(S)
+            stack.append(~node)
+            stack += graph[node]
+    return SCC[::-1]
