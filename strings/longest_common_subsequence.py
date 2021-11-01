a39124b09379bb91234a8438c17df1e93fd331c2
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-11-01 12:02:32 -0400

    Adding a helper function for LCS

---
+++ strings/longest_common_subsequence.py
@@ -0,0 +1,23 @@
+def longestCommonSubsequence(self, a: str, b: str) -> int:
+    lengths = [[0] * (len(b) + 1) for i in range(len(a) + 1)]
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i + 1][j + 1] = lengths[i][j] + 1
+            else:
+                lengths[i + 1][j + 1] = max(lengths[i + 1][j], lengths[i][j + 1])
+
+    result = []
+    x, y = len(a), len(b)
+
+    while x != 0 and y != 0:
+        if lengths[x][y] == lengths[x - 1][y]:
+            x -= 1
+        elif lengths[x][y] == lengths[x][y - 1]:
+            y -= 1
+        else:
+            result.append(a[x - 1])
+            x -= 1
+            y -= 1
+
+    return len(result)
