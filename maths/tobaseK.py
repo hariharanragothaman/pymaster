57fa2724c12f5df03cfa2e24f59e03acf3ca6f95
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-10-28 16:56:39 -0400

    Create tobaseK.py

---
+++ maths/tobaseK.py
@@ -0,0 +1,8 @@
+def toBaseK(self, n: int, k: int) -> str:
+    res = []
+    while n:
+        n, rem = divmod(n, k)
+        res.append(rem)
+    res = res[::-1]
+    res = ''.join(str(c) for c in res)
+    return res
