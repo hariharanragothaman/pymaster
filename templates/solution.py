a08f86360953fc61e2b19f6f173d1e12f252b29b
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-01-06 19:21:35 -0500

    Adding a tester file for LC solution

---
+++ templates/solution.py
@@ -0,0 +1,29 @@
+import os, sys, math, cmath, time, collections
+from math import ceil, floor, log, log2, sqrt, gcd, factorial, pow, pi, log10
+from collections import deque, Counter, OrderedDict, defaultdict
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
+from heapq import nsmallest, nlargest, heapify, heappop, heappush, heapreplace
+from itertools import accumulate, permutations, combinations, combinations_with_replacement
+from io import BytesIO, IOBase
+from functools import reduce
+from typing import *
+
+class Solution:
+    def solve(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    s = Solution()
+
+    # region input
+
+    # endregion
+
+    ans = s.solve()
+    print(ans)
