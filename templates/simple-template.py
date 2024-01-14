7c52ca94ce40e70c980718983903b7437f2d2ebc
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-01-14 18:47:05 -0500

    Simple Template for CSES like websites

---
+++ templates/simple-template.py
@@ -0,0 +1,59 @@
+"""
+செயல் பேசும் ஆழம் இங்கே சொற்கள் பேசுமா?
+
+Focus, Determination and Sheer-Will
+
+The woods are lovely, dark and deep,
+But I have promises to keep,
+And miles to go before I sleep,
+And miles to go before I sleep.
+"""
+
+import os
+import sys
+import time
+
+
+def solve():
+    pass
+
+def test():
+    pass
+
+
+def main() -> None:
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    start_time = time.time()
+
+    solve()
+    debug('-')
+
+    if os.path.exists("data.in"):
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
+
+
+# region debug
+
+def input_as_array() -> list[int]:
+    return list(map(int, input().split()))
+
+
+def debug(char) -> None:
+    if os.path.exists("data.in"):
+        print(char * 25)
+
+
+def debug2(value) -> None:
+    if os.path.exists("data.in"):
+        print(value)
+
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
