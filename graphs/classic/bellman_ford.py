5125ab4e8cc27440dec2227ba54daab186154a64
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-01-18 20:38:22 -0500

    Updating Graph Recipes

---
+++ graphs/classic/bellman_ford.py
@@ -0,0 +1,18 @@
+def bellman_ford(n, edges, start):
+    dist = [float("inf")] * n
+    pred = [None] * n
+
+    dist[start] = 0
+
+    for _ in range(n):
+        for u, v, d in edges:
+            if dist[u] + d < dist[v]:
+                dist[v] = dist[u] + d
+                pred[v] = u
+    """Sanity Check
+    for u, v, d in edges:
+        if dist[u] + d < dist[v]:
+            return None
+    """
+
+    return dist, pred
---
+++ graphs/classic/kruskal.py
@@ -0,0 +1,31 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, a):
+        acopy = a
+        while a != self.parent[a]:
+            a = self.parent[a]
+        while acopy != a:
+            self.parent[acopy], acopy = a, self.parent[acopy]
+        return a
+
+    def merge(self, a, b):
+        self.parent[self.find(b)] = self.find(a)
+
+
+def kruskal(n, U, V, W):
+    union = UnionFind(n)
+    cost, merge_cnt = 0, 0
+    mst_u, mst_v = [], []
+    order = sorted(range(len(W)), key=lambda x: W[x])
+    for i in range(len(W)):
+        u, v = U[order[i]], V[order[i]]
+        find_u, find_v = union.find(u), union.find(v)
+        if find_u != find_v:
+            cost += W[order[i]]
+            merge_cnt += 1
+            union.parent[find_v] = find_u
+            mst_u.append(u), mst_v.append(v)
+
+    return cost, mst_u, mst_v, n == 1 + merge_cnt
