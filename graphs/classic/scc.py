a84f69b860b5f05ada89ebd6075d05b07223e7d0
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-01-18 20:42:09 -0500

    Updating Graph Recipes

---
+++ graphs/classic/scc.py
@@ -0,0 +1,32 @@
+"""
+Given a directed graph, find_SCC returns a list of lists containing
+the strongly connected components in topological order.
+
+Note that this implementation can be also be used to check if a directed graph is a
+DAG, and in that case it can be used to find the topological ordering of the nodes.
+"""
+
+def find_SCC(graph):
+    SCC, S, P = [], [], []
+    depth = [0] * len(graph)
+
+    stack = list(range(len(graph)))
+    while stack:
+        node = stack.pop()
+        if node < 0:
+            d = depth[~node] - 1
+            if P[-1] > d:
+                SCC.append(S[d:])
+                del S[d:], P[-1]
+                for node in SCC[-1]:
+                    depth[node] = -1
+        elif depth[node] > 0:
+            while P[-1] > depth[node]:
+                P.pop()
+        elif depth[node] == 0:
+            S.append(node)
+            P.append(len(S))
+            depth[node] = len(S)
+            stack.append(~node)
+            stack += graph[node]
+    return SCC[::-1]
