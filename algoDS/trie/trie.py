eb9fa9e49cdf0709bb2287d28e594a7fc7573abe
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-01-18 23:13:50 -0500

    Recipes and Polishes

--- algoDS/trie/trie.py
+++ algoDS/trie/trie.py
@@ -59,6 +59,40 @@ class Trie:
             current_dict = current_dict[letter]
         return current_dict["_cnt_"]
 
+class PrefixTree:
+    """
+    This class implements the Trie DataStructure also called as PrefixTree
+    """
+
+    def __init__(self, letter=None):
+        self.letter = letter
+        self.children = {}
+        self.is_word = False
+
+    def insert(self, word):
+        """
+        @brief: Function to insert word into Trie data structure
+        :param: word - word in the dictionary
+        """
+        if len(word):
+            letter = word[0]
+            word = word[1:]
+            if letter not in self.children:
+                self.children[letter] = PrefixTree(letter)
+            return self.children[letter].insert(word)
+        else:
+            self.is_word = True
+            return
+
+    def find_children(self, letter):
+        """
+        @brief: Function to return children of a given letter
+        """
+        if letter not in self.children:
+            return None
+        return self.children[letter]
+
+
 
 if __name__ == "__main__":
     words = ["hello", "world", "wow", "delete", "dell", "hello"]
