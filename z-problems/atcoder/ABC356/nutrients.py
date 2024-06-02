fe48663d97ebebb889fadadbbe760157caebbc4d
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-06-02 15:02:17 -0400

    ABC-356: Nutrients

---
+++ z-problems/atcoder/ABC356/nutrients.py
@@ -0,0 +1,76 @@
+"""
+செயல் பேசும் ஆழம் இங்கே சொற்கள் பேசுமா?
+
+Focus, Determination and Sheer-Will
+
+The woods are lovely, dark and deep,
+But I have promises to keep,
+And miles to go before I sleep,
+And miles to go before I sleep.
+"""
+
+import os
+import sys
+import time
+
+from collections import Counter
+
+def solve():
+    n, m = input_as_array()
+    A = input_as_array()
+
+    H = Counter()
+
+    for i in range(n):
+        x = input_as_array()
+        for i in range(m):
+            H[i] += x[i]
+
+    for i in range(len(A)):
+        if H[i] < A[i]:
+            print("No")
+            return
+
+    print("Yes")
+
+
+def test():
+    pass
+
+
+def main() -> None:
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    start_time = time.time()
+
+    solve()
+    debug('-')
+
+    if os.path.exists("data.in"):
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
+
+
+# region debug
+
+def input_as_array() -> list[int]:
+    return list(map(int, input().split()))
+
+
+def debug(char) -> None:
+    if os.path.exists("data.in"):
+        print(char * 25)
+
+
+def debug2(value) -> None:
+    if os.path.exists("data.in"):
+        print(value)
+
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
