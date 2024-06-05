e07a050085e815a4bd5b4f44aa89a3e024473d7d
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-06-04 20:51:45 -0400

    Container with most water, 2-pointer solution

---
+++ z-problems/leetcode/11.py
@@ -0,0 +1,16 @@
+# Container with Most Water
+class Solution:
+    def maxArea(self, H: List[int]) -> int:
+        n = len(H)
+        ans = 0
+        l, r = 0, n-1
+        max_area = (r-l) * min(H[r], H[l])
+
+        while l < r:
+            if H[l] < H[r]:
+                l += 1
+            else:
+                r -= 1
+            max_area = max(max_area, (r-l)*min(H[r], H[l]))
+
+        return max_area
