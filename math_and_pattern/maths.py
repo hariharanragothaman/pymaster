e2b0796376de96d36cf65c9b5925d557c0186414
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2026-01-20 00:15:36 -0600

    Polishes and updates

--- math_and_pattern/maths.py
+++ math_and_pattern/maths.py
@@ -155,3 +155,21 @@ def pascals_triangle2(n) -> None:
     for i in range(1, n + 1):
         ans.append(ans[-1] * (n - i + 1) // i)
     return ans
+
+def reverse_integer(num):
+    sign = -1 if num < 0 else 1
+    result = 0
+    x = abs(num)
+
+    while x:
+        # Getting the LSB-  multiply by 10 every time;
+        # since it will be correct as we add digits
+        result = (x % 10) + result * 10
+        # Remove the LSB
+        x = x // 10
+
+    # Handle overflow bound scenario
+    if x > 2**31 + 1 or x < -(2**31) - 1:
+        return 0
+
+    return result * sign
