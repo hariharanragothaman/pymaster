5be7f9a11defbcf8491dbc30de3a40391e6f13b7
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2026-01-19 22:40:48 -0600

    Polishes and updates

--- math_and_pattern/maths.py
+++ math_and_pattern/maths.py
@@ -9,92 +9,90 @@ from math import gcd
     print(6 ^ 6 ^ 6 ^ 10 ^ 10) -> 6
 """
 
-class Math:
-    def to_binary(self, n: int, padding: int = 0) -> str:
-        bits = bin(n)[2:]  # remove '0b'
-        return bits.zfill(padding)
+def to_binary(n: int, padding: int = 0) -> str:
+    bits = bin(n)[2:]  # remove '0b'
+    return bits.zfill(padding)
 
-    def factors(self, n) -> set:
-        return set(reduce(list.__add__,([i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0),))
+def factors(n) -> set:
+    return set(reduce(list.__add__,([i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0),))
 
-    def n_c_r(self, n, r):
-        nCr = lambda n, r: reduce(operator.mul, range(n - r + 1, n + 1), 1) // math.factorial(r)
-        return nCr
+def n_c_r(n, r):
+    nCr = lambda n, r: reduce(operator.mul, range(n - r + 1, n + 1), 1) // math.factorial(r)
+    return nCr
 
-    def catalan(self, n):
-        return lambda n: self.n_c_r(2 * n, n) // (n + 1)
+def catalan(n):
+    return lambda n: n_c_r(2 * n, n) // (n + 1)
 
-    @staticmethod
-    def powers_of_two():
-        pow2 = [1 << i for i in range(62)]
-        return pow2
+def powers_of_two():
+    pow2 = [1 << i for i in range(62)]
+    return pow2
 
-    def multiply_by_2(self, n):
-        return n << 1
+def multiply_by_2(n):
+    return n << 1
 
-    def divide_by_2(self, n):
-        return n >> 1
+def divide_by_2(n):
+    return n >> 1
 
-    def two_power_n(self, n):
-        return 1 << n
+def two_power_n(n):
+    return 1 << n
 
-    def count_bits_set_to_one(self, n) -> int:
-        """
-        x << y # Shift to left by y bits -- x * (2**y)
-        x >> y  # Shift to right by y bits -- x // (2**y)
-        Args:
-            n: The I/P number
-        Returns:
-        Logic: And with 1 , and keep dividing n by powers of 2
-        """
-        num_bits = 0
-        while n:
-            num_bits += n & 1
-            n = n >> 1
-            print("n is:", n)
-        return num_bits
+def count_bits_set_to_one(n) -> int:
+    """
+    x << y # Shift to left by y bits -- x * (2**y)
+    x >> y  # Shift to right by y bits -- x // (2**y)
+    Args:
+        n: The I/P number
+    Returns:
+    Logic: And with 1 , and keep dividing n by powers of 2
+    """
+    num_bits = 0
+    while n:
+        num_bits += n & 1
+        n = n >> 1
+        print("n is:", n)
+    return num_bits
 
-    def is_coprime(a, b) -> bool:
-        return gcd(a, b) == 1
+def is_coprime(a, b) -> bool:
+    return gcd(a, b) == 1
 
-    def hamming_distance(self, a, b):
-        x = a ^ b
-        dist = 0
-        while x > 0:
-            dist += x & 1
-            x >>= 1
-        return dist
+def hamming_distance(a, b):
+    x = a ^ b
+    dist = 0
+    while x > 0:
+        dist += x & 1
+        x >>= 1
+    return dist
 
-    def go_over_all_combinations(self, A):
-        n = len(A)
-        for mask in range(1 << n):
-            subset = [A[i] for i in range(n) if mask & (1 << i)]
-            print(subset)
+def go_over_all_combinations(A):
+    n = len(A)
+    for mask in range(1 << n):
+        subset = [A[i] for i in range(n) if mask & (1 << i)]
+        print(subset)
 
-    def apple_divison(self, A, n):
-        total = sum(A)
-        ans = float("inf")
-        limit = 1 << n
+def apple_division(A, n):
+    total = sum(A)
+    ans = float("inf")
+    limit = 1 << n
 
-        # Going through all the combinations generated
-        for msk in range(limit):
-            s = 0
-            # Remember Length of the bit-array
-            for j in range(0, n):
-                # Check and get if the ith bit is set
-                if msk & (1 << j):
-                    s += A[j]
-                current_difference = abs((total - s) - s)
-                ans = min(ans, current_difference)
-        print(ans)
+    # Going through all the combinations generated
+    for msk in range(limit):
+        s = 0
+        # Remember Length of the bit-array
+        for j in range(0, n):
+            # Check and get if the ith bit is set
+            if msk & (1 << j):
+                s += A[j]
+            current_difference = abs((total - s) - s)
+            ans = min(ans, current_difference)
+    print(ans)
 
-if __name__ == '__main__':
-    m = Math()
-    result = m.to_binary(5, 5)
-    print(result)
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
-    f = m.factors(16)
-    print(f)
+# using reduce to find gcd for all multiple numbers at the same time. - you can also pass lambda to it.
+reduce(gcd, [2, 4, 8], 3)
 
-    A = [1, 3, 5, 4]
-    m.go_over_all_combinations(A)
+def lcm(a, b):
+    return a * b / gcd(a, b)
