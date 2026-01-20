a233df26c97a1a3e43fc7b88770683060d27f925
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2026-01-20 00:12:27 -0600

    Polishes and updates

--- math_and_pattern/pascals_triangle.py
+++ math_and_pattern/pascals_triangle.py
@@ -1,29 +1,24 @@
-class Solution:
-    def solve(self, n):
-        op = [[1], [1, 1]]
-        if n == 0:
-            return op[0]
-        elif n == 1:
-            return op[1]
-        i = 2
-        while i < n:
-            candidate = op[-1]
-            temp = []
-            for i in range(1, len(candidate)):
-                temp.append(candidate[i] + candidate[i - 1])
-            temp = [1] + temp + [1]
-            op.append(temp)
+def pascals_triangle(n) -> None:
+    op = [[1], [1, 1]]
+    if n == 0:
+        return op[0]
+    elif n == 1:
+        return op[1]
+    i = 2
+    while i < n:
+        candidate = op[-1]
+        temp = []
+        for i in range(1, len(candidate)):
+            temp.append(candidate[i] + candidate[i - 1])
+        temp = [1] + temp + [1]
+        op.append(temp)
 
-            i += 1
-        return op[-1]
-
+        i += 1
+    return op[-1]
 
 # Another way of doing is as follows:
 # We can build the final answer immediately, using the identity binom(n, i) = binom(n, i - 1) * (n - i + 1) // i.
-
-
-class Solution:
-    def solve(self, n):
+def pascals_triangle2(n) -> None:
         ans = [1]
         for i in range(1, n + 1):
             ans.append(ans[-1] * (n - i + 1) // i)
